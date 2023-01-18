44f4490944f5811f2776a91b7d84609206c080c1
dorreb <ashley_ure@hotmail.com>
2023-01-18 17:07:15 -0500

    adding special keys to send messages to everyone

--- input.js
+++ input.js
@@ -26,6 +26,12 @@ const handleUserInput = function(key) {
   if (key === "d") {
     connection.write("Move: right");
   }
+  if (key === "1") {
+    connection.write("Say: Hi, everyone!");
+  }
+  if (key === "2") {
+    connection.write("Say: Good luck!");
+  }
 };
 
 
