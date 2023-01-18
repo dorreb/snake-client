6208d6118392ca10db27d0bbeeff2de7752793d0
dorreb <ashley_ure@hotmail.com>
2023-01-18 17:55:26 -0500

    adding constants

--- client.js
+++ client.js
@@ -1,29 +1,18 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: '50541'
+    host: IP,
+    port: PORT
   });
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server',);
   });
   conn.write('Name: ASH');
-
-  // setTimeout(() => {
-  //   conn.write('Move: up');
-  // }, 100);
-
-  // setTimeout(() => {
-  //   conn.write('Move: left');
-  // }, 300);
-
-  // setTimeout(() => {
-  //   conn.write('Move: down');
-  // }, 500);
 
   //   // client.js
   conn.on("data", (data) => {
